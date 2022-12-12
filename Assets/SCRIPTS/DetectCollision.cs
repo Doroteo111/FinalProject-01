a0ed63852db97ed55815d243684c490f5d75ccce
Doroteo111 <ladoritaesta@gmail.com>
2022-12-12 19:56:49 +0100

    not yet finished
    - arreglar lo del contador de la bola
    -que la roca este durísima
    -limpiar y acabar proyecto

--- Assets/SCRIPTS/DetectCollision.cs
+++ Assets/SCRIPTS/DetectCollision.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class DetectCollision : MonoBehaviour
 {
     private int result;
-    private void OnTriggerEnter(Collider other)  //the sphere collision wit the coin, and it destroyed
+    private void OnTriggerEnter(Collider other)  //the sphere collision wit the mushroom, and it destroyed
     {
-        if (other.gameObject.name.Contains("coin"))
+        if (other.gameObject.name.Contains("Mushroom"))
         {
             Destroy(other.gameObject);
             result++;
-            Debug.Log($"You have collected {result} coins");
+            Debug.Log($"You have collected {result} Mushroom");
             Victory();
         }
        
--- Assets/SCRIPTS/PlayerControl.cs
+++ Assets/SCRIPTS/PlayerControl.cs
@@ -5,18 +5,10 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
 
-    public float speed = 20f;
-    public float turnSpeed = 50f; //speed
+    private float speed = 20f;
+    private float turnSpeed = 60f; //speed
 
     private float horizontalInput, verticalInput; //movment
-
-    /*private int points = 1; //couting coins collected
-    private int result;*/
-
-    /*private void Start()
-    {
-        Counter(points);  
-    }*/
 
     private void Update()
     {
@@ -33,20 +25,5 @@ public class PlayerControl : MonoBehaviour
             
     }
 
-    /*private void Counter(int x)
-    {
-        result = x++;
-        if (true)
-        {
-            Debug.Log($"You have collected {result} coins");
-        }
-    }*/
-
-    /*private void Victory()
-    {
-        if (result == 30)
-        {
-            Debug.Log("YOU WIN!!");
-        }
-    }*/
+    
 }
