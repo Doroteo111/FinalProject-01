1e710893c9a29c1b7a1e4b5dcac336f4110a3fb3
Doroteo111 <ladoritaesta@gmail.com>
2022-12-12 10:48:46 +0100

    objetivos completados
    -->falta limpiar codigo y comentarlo
    -->decoración assets

--- Assets/SCRIPTS/DetectCollision.cs
+++ Assets/SCRIPTS/DetectCollision.cs
@@ -4,34 +4,28 @@ using UnityEngine;
 
 public class DetectCollision : MonoBehaviour
 {
-    private int points = 1; //couting coins collected
     private int result;
     private void OnTriggerEnter(Collider other)  //the sphere collision wit the coin, and it destroyed
     {
         if (other.gameObject.name.Contains("coin"))
         {
             Destroy(other.gameObject);
+            result++;
+            Debug.Log($"You have collected {result} coins");
+            Victory();
         }
        
     }
 
-    private void Start()
-    {
-        Counter(points);
-    }
-    private void Counter(int x)
-    {
-        result = x++;
-        if (true)
-        {
-            Debug.Log($"You have collected {result} coins");
-        }
-    }
+   
+    
     private void Victory()
     {
         if (result == 30)
         {
             Debug.Log("YOU WIN!!");
+            Time.timeScale = 0;
         }
     }
+    
 }
