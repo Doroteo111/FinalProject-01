64f1c96ec68029adb0c58ae58edbd6c7a8f04035
Doroteo111 <ladoritaesta@gmail.com>
2022-12-13 12:12:29 +0100

    FINISHED

--- Assets/SCRIPTS/DetectCollision.cs
+++ Assets/SCRIPTS/DetectCollision.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class DetectCollision : MonoBehaviour
 {
     private int result;
-    private void OnTriggerEnter(Collider other)  //the sphere collision wit the mushroom, and it destroyed
+    private void OnTriggerEnter(Collider other)  //The mashroom will disapear when the ball touch it
     {
-        if (other.gameObject.name.Contains("Mushroom"))
+        if (other.gameObject.name.Contains("Mushroom")) //every prefab with the same name or contain the name will obey the if
         {
             Destroy(other.gameObject);
             result++;
-            Debug.Log($"You have collected {result} Mushroom");
+            Debug.Log($"You have collected {result} Mushroom"); //show the amunt of mashroom you have during the gameplay
             Victory();
         }
        
@@ -19,7 +19,7 @@ public class DetectCollision : MonoBehaviour
 
    
     
-    private void Victory()
+    private void Victory() //when you ontain 30 mashroom,a message will shown and the screen stops
     {
         if (result == 30)
         {
--- Assets/SCRIPTS/PlayerControl.cs
+++ Assets/SCRIPTS/PlayerControl.cs
@@ -6,23 +6,20 @@ public class PlayerControl : MonoBehaviour
 {
 
     private float speed = 20f;
-    private float turnSpeed = 60f; //speed
+    private float turnSpeed = 60f; 
 
-    private float horizontalInput, verticalInput; //movment
+    private float horizontalInput, verticalInput; //movment direction
 
     private void Update()
     {
-        //Movment
+        //Movment with w,a,s,d keys
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
 
         transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
-        /*transform.Translate(Vector3.right * speed * Time.deltaTime * horizontalInput);*/
-
         transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
-
-            
+ 
     }
 
     
